0a07f93fbeafe1d51e27fbd817b09c76d66a41c1
luciabgms <lucia.ballesteros.grande@gmail.com>
2022-11-16 10:39:46 +0100

    Addition of generating TLE function

--- generating_TLE_function.py
+++ generating_TLE_function.py
@@ -128,3 +128,91 @@ def MeanMotion(a):
     mu = 3.986E5 # standard gravitational parameter of Earth [km^3/s^2]
     MM = T_day/(2*np.pi)*np.sqrt(mu/a**3)
     return MM
+
+def checksum_mod10(variable):
+    counter = 0
+    for c in variable:  
+        if c == "-":
+            counter += 1
+        elif c.isnumeric():
+            counter += int(c)
+    checksum = str(counter % 10)
+    return checksum
+
+def Generate_TLE(Inclination, RAAN, Eccentricity, AOP, MeanAnomaly, MeanMotion, MeanMotionDot, 
+MeanMotionDDot, BSTAR, Epoch, Satellite_catalog_number,
+Revolution_number, Classification, International_designator, Element_set_number):
+    """
+    Generates realistics Two-Line Elements (TLE) for notional systems
+    Reference: ROCKWOOD, Troy; STEEGER, Greg; STEIN, Matthew. Generating Realistic Two-Line Elements for Notional 
+    Space Vehicles and Constellations. arXiv preprint arXiv:2203.04204, 2022.
+
+    Parameters
+    ----------
+    Inclination: string
+        inclination of the orbit [deg]
+    RAAN: string
+        right ascension of the ascending node [deg]
+    Eccentricity:string
+        eccentricity of the orbit [-]
+    AOP: string 
+        argument of perigee [deg]
+    MeanAnomaly: string
+        mean anomaly of the orbit [deg]
+    MeanMotion: string
+        mean motion of the satellite [rev/day]
+    MeanMotionDot: string (+.XXXXXXXX)
+        first time derivative of the mean motion []
+    MeanMotionDDot: string (+XXXXX-X)
+        second time derivative of the mean motion []
+        leading decimal point assumed
+    BSTAR: string (+XXXXX-X)
+        drag term []
+        leading decimal point assumed
+    Epoch: string (XXYYY.YYYYYYYY)
+        XX: last two digits of year
+        YYY.YYYYYYYY: day of the year and fractional portion of the day
+    Satellite_catalog_number: string
+        -
+    Revolution_number: string
+        Revolution number at epoch [revs]
+    Classification: string
+        U for unclissified
+    International_designator: string (XXYYYZZZ)
+        XX: last two digits of launch year
+        YYY: launch number of the year
+        ZZZ: piece of the launch
+    Element_set_number: string
+        - 
+    
+    Notes: 
+    * The classification, international designator value, element set number and revolution number can
+    all be chosen at will, do not affect the simulation
+    * The satellite catalog number should not conflict with other satellites simulations
+    * The ephemeris type has been set to 0 
+    * The epoch can be chosen at will but with some considerations. If working with notional systems, then select similar epochs.
+    If mixing notional with real satellites, choose an epoch close to the real systems of interest. 
+    * The inclination, RAAN, eccentricity, AOP, and mean anomaly are all chosen according to the desired simulation.
+    * The first and second time derivatives of the mean motion and, the drag term can be derived from existing TLE. This terms are 
+    important to obtain realistic TLE. 
+
+    Returns
+    -------
+    TLE : string
+        TLE of a notional system
+    """
+
+    TLE1 = "1" + " " + Satellite_catalog_number + Classification + " " + International_designator + " " + Epoch + " "
+    TLE1 += MeanMotionDot + " " + MeanMotionDDot + " " + BSTAR + " " + "0" + Element_set_number 
+
+    TLE2 = "2" + " " + Satellite_catalog_number + " " + Inclination + " " + RAAN + " " + Eccentricity + " " + AOP + " " 
+    TLE2 += MeanAnomaly + " " + MeanMotion + " " + Revolution_number
+    
+    TLE1 += checksum_mod10(TLE1)
+    TLE2 += checksum_mod10(TLE2)
+
+    TLE = (TLE1, TLE2)
+    return(TLE)
+
+
+
