114ac8da0cf11451f73c79d1cb243da6083484f1
luciabgms <lucia.ballesteros.grande@gmail.com>
2023-01-15 13:50:37 +0100

    Add coments + fix minor errors

--- FoM_functions.py
+++ FoM_functions.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 from astropy import units as u
 import csv
+from poliastro.core.angles import E_to_nu
 
-from TLE_OP_function import from_TLE_to_OrbParams, E_and_TA_from_MA, time_in_sat_epoch, time_in_constellation_epoch, prop_to_start_time
+from TLE_OP_function import from_TLE_to_OrbParams, E_and_TA_from_MA, time_in_sat_epoch, time_in_constellation_epoch 
+from TLE_OP_function import prop_to_start_time, from_t_to_E_TA
 from scipy.optimize import fsolve
 
 # DATOS
@@ -11,41 +13,61 @@ Rt = 6378 # km
 mu = 3.986e5 # km3/s2
 
 def elevacion_sat(time_span, lat_GS, long_GS, constellation):
-    # Obtengo los parámetros orbitales para la última actualización del TLE del satélite
+    """
+    Cálculo de la elevación en cada satélite en una constelación para unos instantes temporales y una GS
+
+    Parámeteros
+    ----------
+    time_span : float array
+        instantes temporales de evaluación de los contactos
+    lat_GS: float
+        latitud de la Ground Station [deg]
+    constellation : list of strings
+        names of the satellites in the constellation
+
+    Devuelve
+    -------
+    eps_sat : float array
+        Elevación del satélite en cada instante temporal [rad]
+    """
+
+    # Obtengo los parámetros orbitales para la última actualización del TLE de cada satélite
     RAAN_deg, i_deg, e, a, AOP_deg, MA_deg = from_TLE_to_OrbParams(constellation)    # dim (sat, )
 
-    # paso los datos de los parámetros orbitales a radianes para utilizar np.cos, np.sin, np.tan, etc.
+    # Paso los datos de los parámetros orbitales a radianes para utilizar np.cos, np.sin, np.tan, etc.
     RAAN =  np.deg2rad(RAAN_deg) # rad, dim (sat, )
     i =  np.deg2rad(i_deg) # rad, dim (sat, )
     AOP =  np.deg2rad(AOP_deg) # rad, dim (sat, )
     MA =  np.deg2rad(MA_deg) # rad, dim (sat, )
 
+    # Para el instante de la actualización de cada TLE, calculo la anomalía excéntrica (E0) y el tiempo respecto del paso 
+    # por el perigeo (t0) con la ecuación de Kepler. Cálculo del periodo orbital (T) de cada satélite
     E0_rad, TA0_rad = E_and_TA_from_MA(MA, e) # rad, dim (sat, )
     t0 = (E0_rad-e*np.sin(E0_rad))*np.sqrt(a**3/mu)
     T = 2*np.pi*np.sqrt(a**3/mu)
 
-    t_in_orbit, max_t_index, delta_t = prop_to_start_time(constellation, t0, T)
-
-    # t_contact_eval = t_in_orbit.reshape(1, len(t_in_orbit)) + time_span.reshape(len(time_span),1) #dim (t, sat)
-
-    E_in_orbit = np.zeros_like(e) # dim (sat, )
-    for k, (ecc,sma) in enumerate(zip(e,a)):
-        t = t_in_orbit[k]
-        # Hallo la anomalía excéntrica (rad) para cada instante de tiempo y para cada satélite
-        def f(x):
-            return x - ecc*np.sin(x) - np.sqrt(mu/sma**3)*t
-        E_in_orbit[k] = fsolve(f, 0) # rad
-
-    # hasta aquí tenemos unos satélites de los que conocemos sus parámetros orbitales a través de sus TLEs
-    # los TLEs no se actualizan al mismo tiempo, así que hallamos el satélite que ha sido actualizado hace menos tiempo y vemos 
-    # cuánto tiempo falta por propagar al resto para llegar hasta ese instante temporal en segundos (span_difference_array)
+    # Hasta aquí tenemos unos satélites de los que conocemos sus parámetros orbitales a través de sus TLEs.
+    # Los TLEs no se actualizan al mismo tiempo, así que hallamos el satélite que ha sido actualizado hace menos tiempo y vemos 
+    # cuánto tiempo falta por propagar al resto para llegar hasta ese instante temporal en segundos (delta_t).
     # Con el dato de la MA del TLE sacamos la E del instante del TLE (E0) y hallamos el tiempo para el que sucede (t0)
     # Con el instante inicial y lo que falta para llegar al momento temporal del último satélite actualizado hallamos donde está
-    # el satélite en el momento en el que vamos a empezar a evaluar los contactos (t_in_orbit)
-    # para ese instante temporal obtenemos la E (E_in_orbit) para comparar con plot_TLE_OP.ipynb si la distribución 
-    # de satélites es coherente
+    # el satélite en el momento en el que vamos a empezar a evaluar los contactos (t_in_orbit).
+    # Para ese instante temporal obtenemos la E (E_in_orbit).
+    # Se puede comparar con plot_TLE_OP.ipynb si la distribución de satélites es coherente.  
+      
+    t_in_orbit, max_t_index, delta_t = prop_to_start_time(constellation, t0, T)
+    E_in_orbit, TA_in_orbit = from_t_to_E_TA(e, a, t_in_orbit)
+
+    E_in_orbit = np.zeros_like(e) # dim (sat, )
 
     w_rot_T = 2*np.pi/(24*3600) # rad/s
+
+    # Ahora se va a realizar la propagación de los satélites con la ecuación de Kepler para evaluar los contactos. 
+    # Para ello, para cada satélite se halla la anomalía excéntrica en unos instantes temporales (time_span) desde 0 hasta el
+    # tiempo que se halla definido en FoM_main.py . Como el tiempo t = 0 s representa el paso del satélite por el perigeo, se
+    # debe sumar la anomalía excéntrica a la que se encontraba el satélite antes de empezar la evaluación de los contactos 
+    # (E_in_orbit). El resultado de la propagación con la ecuación de Kepler (E) se emplea, por un lado, para saber la anomalía
+    # excéntrica en cada punto de la propagación (E_rad = E + E_in_orbit) y, por otro, para hallar la anomalía verdadera (TA_rad)
 
     E = np.zeros( ( len(time_span), len(MA)) ) # dim (t, sat )
     for k, (ecc,sma) in enumerate(zip(e,a)):
@@ -56,18 +78,25 @@ def elevacion_sat(time_span, lat_GS, long_GS, constellation):
             E[index, k] = fsolve(f, 0) # rad
     E_rad = np.where(E<0, E+2*np.pi, E)
     E_rad += E_in_orbit
-    # Hallo el radio (km) y la anomalía verdadera (rad)
+
+    # Hallo el radio (km) y la anomalía verdadera (rad) en cada instante temporal para cada satélite
     r = a*(1-e*np.cos(E_rad)) # km, dim (t, sat)
     TA_rad = 2*np.arctan(np.sqrt((1+e)/(1-e))*np.tan(E/2)) # rad, dim (t, sat)
     TA_rad = np.where(TA_rad<0, TA_rad+2*np.pi, TA_rad)
-    TA_rad += E_in_orbit # TODO
 
-    # Hallo el tiempo sidereo local de la estación 
+    TA_rad += TA_in_orbit 
+
+    # Hallo el tiempo sidereo local de la estación en el inicio de la evaluación de los contactos --> en el momento 
+    # temporal del satélite que ha actualizado hace menos tiempo su TLE (GS_time). GS_time es un objeto de astropy.time que tiene 
+    # asociado un atributo que convierte el tiempo dado a TLS para una longitud determinada ( .sideral_time() ). El resultado de 
+    # este atributo es un tiempo en horas, minutos y segundos que para el problema que se está evaluando hay que pasar a radianes.
+    # Además, se necesita un LST de la GS para cada instante de evaluación de los contactos --> GS_LST_rad
     GS_time = time_in_sat_epoch(constellation[max_t_index])
     GS_LST_deg = GS_time.sidereal_time('mean', longitude=long_GS*u.deg).value # h, min, s; 
     GS_LST_rad = GS_LST_deg*15*np.pi/180 + w_rot_T*time_span # dim (t, )
 
-    # Ec(24) apartado 6.4 Elices, dim r_dot_cospsi (t, sat)
+    # Con todos estos datos se calcula la elevación del satélite 
+    # Ec(24) apartado 6.4 Elices. dim r_dot_cospsi (t, sat)
     lat_GS = np.deg2rad(lat_GS)
     GS_LST_rad = GS_LST_rad.reshape(len(GS_LST_rad),1) # dim (t, 1)
     r_dot_cospsi = r*( np.cos(lat_GS)*( np.cos(AOP+TA_rad)*np.cos(GS_LST_rad-RAAN) + np.cos(i)*np.sin(AOP+TA_rad)*np.sin(GS_LST_rad-RAAN) ) + np.sin(lat_GS)*np.sin(i)*np.sin(AOP+TA_rad) )
@@ -78,6 +107,15 @@ def elevacion_sat(time_span, lat_GS, long_GS, constellation):
     return eps_sat, delta_t
 
 def GMAT_parameters(constellation):
+    """
+    Print de una tabla con los parámetros orbitales de cada satélite en la constelación y su epoch en UTC Gregorian
+
+    Parámeteros
+    ----------
+    constellation : list of strings
+        names of the satellites in the constellation
+    """
+    
     RAAN_deg, i_deg, e, a, AOP_deg, MA_deg = from_TLE_to_OrbParams(constellation)    # dim (sat, )
 
     _, TA0_rad = E_and_TA_from_MA(np.deg2rad(MA_deg), e) # rad, dim (sat, )
@@ -90,10 +128,26 @@ def GMAT_parameters(constellation):
     
     tabla = pd.DataFrame(data, columns=["Satelite", "SMA [km]", "ECC", "INC [deg]", "RAAN [deg]", "AOP [deg]", "TA0 [deg]", "Epoch in UTC Gregorian"])
     print(tabla)
-    tabla.to_csv('outputs/GMAT_parameters.csv')
+    # tabla.to_csv('outputs/GMAT_parameters.csv')
 
-def contact_locator(eps_sat, eps_GS, time_span, constellation, delta_t):
+def contact_locator(eps_sat, eps_GS, time_span, sat_name, delta_t):
+    """
+    Print de una tabla con la información de los contactos para un satélite dado
 
+    Parámeteros
+    ----------
+    eps_sat : float array
+        elevación del satélite en cada instante temporal de evaluación de los contactos
+    eps_GS: float
+        elevación mínima de la Ground Station [deg]
+    time_span : float array
+        instantes temporales de evaluación de los contactos
+    sat_name: string
+        nombre del satélite del que se están evaluando los contactos
+    delta_t: float
+        desfase temporal desde la actualización del TLE del satélite hasta el inicio de la evaluación de los contactos
+    """
+    
     eps_GS= np.deg2rad(eps_GS) # rad
     status = np.where((eps_sat >= eps_GS) & (eps_sat <= np.pi- eps_GS), 1, 0) # dim (t, sat)
 
@@ -106,24 +160,12 @@ def contact_locator(eps_sat, eps_GS, time_span, constellation, delta_t):
             contact_locator.append(contact[j])
     contact_locator.append(contact[-1])
 
-    start_time = time_in_sat_epoch(constellation) + delta_t*u.s
+    start_time = time_in_sat_epoch(sat_name) + delta_t*u.s
     start_time.format = 'isot'
     
-    duration_vec = []
-    with open(f'outputs/{constellation}.csv', 'w') as csvfile:
-        fieldnames = ['Start time', 'Stop time', 'Duration [s]']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    
-        for j in range(0,len(contact_locator),2):
-            d = {
-                "Start time": start_time + time_span[contact_locator[j]]*u.s, 
-                "Stop time": start_time + time_span[contact_locator[j+1]]*u.s,
-                "Duration [s]": time_span[contact_locator[j+1]]-time_span[contact_locator[j]]
-             }
-            writer.writerow(d)
-            duration_vec.append(time_span[contact_locator[j+1]]-time_span[contact_locator[j]])
-
-    print("Para el satélite", constellation, ": start time =", start_time)            
-    print("Number of contacts =", len(contact_locator)/2)
-    print("Duración de los contactos [s]:", duration_vec)
+    data = []
+    for j in range(0,len(contact_locator),2):
+        data.append([ sat_name, start_time + time_span[contact_locator[j]]*u.s, start_time + time_span[contact_locator[j+1]]*u.s, time_span[contact_locator[j+1]]-time_span[contact_locator[j]] ])
+    tabla = pd.DataFrame(data, columns=["Satellite", "Start Time [s]", "Stop Time [s]", "Duration [s]"])
+    print(tabla)
     print("-----------------------------------------------------")
--- TLE_OP_function.py
+++ TLE_OP_function.py
@@ -60,8 +60,27 @@ def from_TLE_to_OrbParams(constellation):
     return RAAN, i, e, a, AOP, MA
 
 def E_and_TA_from_MA(MA_rad, e):
+    """
+    Transforms Mean Anomaly into Excentric Anomaly and True Anomaly.
+
+    Parameters
+    ----------
+    MA_rad : float array
+        Mean Anomaly [rad]
+    e : float array
+        Eccentricity [-]
+
+    Returns
+    -------
+    E_rad : float array
+        Excentric Anomaly [rad]
+    TA_rad: float array
+        True Anomaly [rad]
+    """
+
     E_rad = np.zeros_like(MA_rad)
     TA_rad = np.zeros_like(MA_rad)
+
     for index in range(len(MA_rad)):
         E_rad[index] = M_to_E(MA_rad[index], e[index]) # dim (sat, )
         TA_rad[index] = E_to_nu(E_rad[index], e[index]) # dim (sat, )
@@ -70,23 +89,70 @@ def E_and_TA_from_MA(MA_rad, e):
     return E_rad, TA_rad
 
 def time_in_sat_epoch(sat_name):
+    """
+    Gives the epoch time in UTC of the last update of a determined satellite
 
+    Parameters
+    ----------
+    sat_name : string
+        Name of the sat for which the time is desired
+
+    Returns
+    -------
+    time : time object
+        Epoch time for the satellite in UTC
+    """
     sat = list(load_gp_from_celestrak(name=sat_name))[0]
     sat_dict = sat_to_dict(sat,sat_name)
     time = Time(sat_dict["EPOCH"], scale='utc')
-    # time =  Time('2023-01-02T15:46:51.437', scale='utc') #TODO
+
     return time
 
 def time_in_constellation_epoch(constellation):
+    """
+    Gives the epoch time in UTC of the last update of each satellite in a constellation
+
+    Parameters
+    ----------
+    constellation : string array
+        Name of the satellites in the constellation for which the times are desired
+
+    Returns
+    -------
+    time : list of time objects
+        Epoch time for the satellite in UTC
+    """
+
     time = []
     for sat_name in constellation:
         sat = list(load_gp_from_celestrak(name=sat_name))[0]
         sat_dict = sat_to_dict(sat,sat_name)
         time.append(Time(sat_dict["EPOCH"], scale='utc'))
-    # time =  Time('2023-01-02T15:46:51.437', scale='utc') #TODO
+
     return time
 
 def prop_to_start_time(constellation, t0, T):
+    """
+    Given a constellation, calculates the most recent update of the TLE in the constellation and gives the parameters nedeed to propagate
+    the rest of the satellites to that time.
+
+    Parameters
+    ----------
+    constellation : string array
+        Name of the satellites in the constellation for which the times are desired
+    t0: float array
+        Time elapsed [s] from perigee passage to TLE update time for each satellite in a constellation
+    T: float array
+        Orbital period for each satellite in a constellation [s]
+    Returns
+    -------
+    t_in_orbit : float array
+        Time elapsed [s] from perigee passage to start contact evaluation time for each satellite in a constellation
+    max_t_index : integer
+        Index of the satellite in the constellation which has the most recent update of the TLE
+    span_difference_array : float array   
+        Propagation time needed for each satellite to reach the epoch time of the satellite which has the most recent update of the TLE
+    """
 
     constellation_times = time_in_constellation_epoch(constellation)
     span_difference = []
@@ -100,3 +166,40 @@ def prop_to_start_time(constellation, t0, T):
     t_in_orbit = (t0 + span_difference_array)%T
 
     return t_in_orbit, max_t_index, span_difference_array
+
+def from_t_to_E_TA(e, a, t_in_orbit):
+    """
+    For a given time and a determined orbit, gives the excentric anomaly and the true anomaly at that time
+
+    Parameters
+    ----------
+    a: float array
+        Semi-major axis [km]
+    e: float array
+        Eccenticity [-]
+    t_in_orbit : float array
+        Time elapsed [s] from perigee passage to start contact evaluation time for each satellite in a constellation
+
+    Returns
+    -------
+    E_in_orbit : float
+        Excentric Anomaly [rad]
+    TA_in_orbit : float 
+        True Anomaly [rad]
+        
+    """
+
+    E_in_orbit = np.zeros_like(e) # dim (sat, )
+    for k, (ecc,sma) in enumerate(zip(e,a)):
+        t = t_in_orbit[k]
+        # Hallo la anomalía excéntrica (rad) para cada instante de tiempo y para cada satélite
+        def f(x):
+            return x - ecc*np.sin(x) - np.sqrt(mu/sma**3)*t
+        E_in_orbit[k] = fsolve(f, 0) # rad
+
+    TA_in_orbit = np.zeros_like(E_in_orbit)
+    for index in range(len(E_in_orbit)):
+        TA_in_orbit[index] = E_to_nu(E_in_orbit[index], e[index]) # dim (sat, )
+    TA_in_orbit = np.where(TA_in_orbit <0 , TA_in_orbit + 2*np.pi, TA_in_orbit)
+
+    return E_in_orbit, TA_in_orbit
